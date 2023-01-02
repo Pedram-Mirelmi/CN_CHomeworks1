549e582edda8951a9add44a1c744a5cd3ab99256
pedram <mirelmipedram@gmail.com>
2023-01-02 17:55:55 +0330

    removed HTTP files from Chat-server

--- HTTP/AsyncServerFramework/INetWriter.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "./io/BasicHTTPMessage.h"
-#include "./Session.h"
-
-class INetWriter : public std::enable_shared_from_this<INetWriter>
-{
-public:
-    virtual void writeMessage(shared_ptr<HTTPMessage> msg, std::shared_ptr<Session> session) = 0;
-};
--- HTTP/INetMessageProcessor.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "./AsyncServerFramework/io/BasicHTTPMessage.h"
-#include "AsyncServerFramework/Session.h"
-
-class INetMessageProcessor : public std::enable_shared_from_this<INetMessageProcessor>
-{
-public:
-    virtual void processNetMessage(shared_ptr<HTTPMessage> netMsg, shared_ptr<Session> session) = 0;
-};
